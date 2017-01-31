7dd64c2682139356148df8886938a5d7fca75bee
Chris Alexander <chris@chris-alexander.co.uk>
2017-01-31 16:52:17 +0000

    NYT links

---
+++ nyt.fsx
@@ -0,0 +1,7 @@
+// NY PDF: http://www.nytimes.com/images/2017/01/25/nytfrontpage/scan.pdf
+// National PDF: http://www.nytimes.com/images/2017/01/25/nytfrontpage/scannat.pdf
+// International PDF: http://www.nytimes.com/images/2017/01/25/nytfrontpage/INYT_frontpage_global.20170125.pdf
+
+// NY JPG: https://static01.nyt.com/images/2017/01/25/nytfrontpage/scan_paper.jpg
+// National JPG: https://static01.nyt.com/images/2017/01/25/nytfrontpage/scannat_paper.jpg
+// International JPG: https://static01.nyt.com/images/2017/01/25/nytfrontpage/INYT_frontpage_global.20170125.jpg 
