11d72053b7b664c4a535c6662831fda15158d850
Chris Alexander <chris@chris-alexander.co.uk>
2017-02-17 17:50:34 +0000

    NYT linklauncher

--- nyt.fsx
+++ nyt.fsx
@@ -5,3 +5,43 @@
 // NY JPG: https://static01.nyt.com/images/2017/01/25/nytfrontpage/scan_paper.jpg
 // National JPG: https://static01.nyt.com/images/2017/01/25/nytfrontpage/scannat_paper.jpg
 // International JPG: https://static01.nyt.com/images/2017/01/25/nytfrontpage/INYT_frontpage_global.20170125.jpg 
+
+type Edition =
+    | NewYork
+    | National
+    | International
+
+type Format =
+    | PDF
+    | JPG
+
+let getFormatPath (date : System.DateTime) format =
+    match format with
+    | PDF -> sprintf "http://www.nytimes.com/images/%i/%02i/%02i/nytfrontpage/" date.Year date.Month date.Day
+    | JPG -> sprintf "https://static01.nyt.com/images/%i/%02i/%02i/nytfrontpage/" date.Year date.Month date.Day
+
+let getFileExtension format =
+    match format with
+    | PDF -> "pdf"
+    | JPG -> "jpg"
+
+let getFileRoot (date : System.DateTime) edition =
+    match edition with
+    | NewYork       -> "scan"
+    | National      -> "scannat"
+    | International -> sprintf "INYT_frontpage_global.%i%02i%02i" date.Year date.Month date.Day
+
+let getFile date edition format =
+    let path = getFormatPath date format
+    let fileRoot = getFileRoot date edition
+    let extension = getFileExtension format
+    path + fileRoot + "." + extension
+
+let getFileForToday =
+    getFile System.DateTime.Now
+
+let getToday =
+    getFileForToday NewYork PDF
+
+let openLink (url : string) =
+    ignore (System.Diagnostics.Process.Start(url))
